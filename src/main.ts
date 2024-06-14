2cfbfd727e9ba61dfb1d3b13c396d1028dac72ef
Qyokizzzz <1104597683@qq.com>
2024-06-14 13:46:19 +0800

    build: vite

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,35 @@
+import { join } from 'path';
+// import { createServer as createViteServer } from 'vite';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import type { NestExpressApplication } from '@nestjs/platform-express';
+// import type { Request, Response, NextFunction } from 'express';
+// import type { ViteDevServer } from 'vite';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  // app.useStaticAssets(join(__dirname, '..', 'client'));
+
+  // const vite = await createViteServer({
+  //   server: { middlewareMode: true },
+  //   appType: 'custom',
+  // });
+
+  // app.use(
+  //   (
+  //     req: Request & { viteServer: ViteDevServer },
+  //     _: Response,
+  //     next: NextFunction,
+  //   ) => {
+  //     req['viteServer'] = vite;
+  //     next();
+  //   },
+  // );
+
+  // app.use(vite.middlewares);
+
   await app.listen(4000);
 }
 bootstrap();
---
+++ vite.config.js
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite';
+// import { fileURLToPath, URL } from 'url';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+
+export default defineConfig({
+  publicDir: resolve(__dirname, 'client', 'public'),
+  plugins: [
+    react({
+      babel: {
+        plugins: [['@babel/plugin-transform-react-jsx']],
+      },
+      jsxRuntime: 'classic',
+    }),
+  ],
+  build: {
+    minify: true,
+    emptyOutDir: false,
+  },
+  ssr: {
+    format: 'cjs',
+  },
+  legacy: {
+    buildSsrCjsExternalHeuristics: true,
+  },
+});
